0b6128030574df130eef0b80c570a1113e0a3b69
Thorfinn3 <charles.picard3@gmail.com>
2021-02-09 11:33:00 +0100

    Update capteurs.ino

--- capteur/capteurs.ino
+++ capteur/capteurs.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
 
 //--------------------DECLARATIONS DE VARIABLES---------------------------
 
@@ -8,6 +9,7 @@
 
 //Definition des structures de donnees pour les differents module
 SoftwareSerial BTSerie(RxD,TxD); //Module bluetooth 1
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 float volt; //Voltage du capteur de temperature
 float temperature; //Temperature du salon
@@ -36,6 +38,8 @@ void setup() {
 
   InitCommunicationSerie() ; //Initialisation de la carte
   Serial.println("Initialisation carte : Ok");
+  lcd.init();
+  Serial.println("Initialisation lcd : Ok");
 
   asktemp = false;
   chauffageAuto = true;
@@ -49,6 +53,10 @@ void setup() {
   InitCommunicationBluetoothSerie();
   
   startMillis = millis(); //initialisation du temps
+
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Bonjour vous :)");
 }
 
 //----------------------------------------------------------------------------------
@@ -61,42 +69,54 @@ void loop() {
   char input;
   String test;
   
-
   currentMillis = millis();  //Reccuperation du temps actuel
    readSerialPort();
    if(msg!=""){
      Serial.print(msg);
-     if(msg[0] == 't'){
+     //Traitement en fonction des messages
+     if(msg[0] == 't'){ //Reccuperation de la temperature
         asktemp = true;
         temperature = getTemp();
         Serial.println(temperature);
-     } else if(msg[0] == 'a' && !chauffageAllume) {
+        affichageLCD(temperature, chauffageAllume);
+     } else if(msg[0] == 'a') { //Allumage de la lumiere
         chauffageAllume = true;
         digitalWrite(ledChauffage, HIGH); //Allume la led       
-     } else if(msg[0] == 'e' && chauffageAllume) {
+     } else if(msg[0] == 'e') { //Extinction de la lumiere
         chauffageAllume = false;
         digitalWrite(ledChauffage, LOW); //Eteint la led
-     } 
+     } else if(msg[0] == 'c' && !chauffageAllume) { //Allumage du chauffage
+        asktemp = true;
+        temperature = getTemp();
+        chauffageAllume = true;
+        affichageLCD(temperature, chauffageAllume);
+     } else if(msg[0] == 's' && chauffageAllume){ //Extinction du chauffage
+        asktemp = true;
+        temperature = getTemp();
+        chauffageAllume = false;
+        affichageLCD(temperature, chauffageAllume);
+     }
      msg=""; 
    }
 
   //Si la temperature du salon est demandee ou qu'il s'est passe environs 5 min
    if(asktemp == true || currentMillis - startMillis >= 180000) {
     asktemp = false;
+    temperature = getTemp();
     BTSerie.println(temperature); //On affiche la temperature sur le potable
     photoresCatch(); //Met a jour le min et le max en fonction de la luminosite
 
     //Verification du depassement des temperatures max et min et si le chauffage est deja en cours d'utilisation ou non
     if(temperature >= tempMax && chauffageAllume) {
-          //Si la temperature max est depasse on propose d'eteindre le chauffage
+          //Si la temperature max est depasse on eteint le chauffage
           BTSerie.println("La temperature max est depassee dans le salon, le chauffage va s'eteindre");
           chauffageAllume = false;
-          digitalWrite(ledChauffage, LOW); //Eteint la led
+          affichageLCD(temperature, chauffageAllume);
     } else if (temperature <= tempMin && !chauffageAllume) {
-          //Si la temperature min est depassee on propose d'allumer le chauffage
+          //Si la temperature min est depassee on allume le chauffage
           BTSerie.println("La temperature minimal est depassee dans le salon, le chauffage va s'allumer");
           chauffageAllume = true;
-          digitalWrite(ledChauffage, HIGH); //Allume la led   
+          affichageLCD(temperature, chauffageAllume);
     }
     
     startMillis = currentMillis;  //Mise a jour du timer
@@ -111,6 +131,7 @@ void loop() {
 
 //--------------------Fonctions utilitaires--------------------------
 
+//Fonction permettant de reccuperer la temperature
 float getTemp(){
    //Reccuperation de la temperature
     volt = analogRead(0); //On lit les données du capteur
@@ -118,8 +139,8 @@ float getTemp(){
     return volt/0.01; //Calcule de la temperature en fonction du voltage
 }
 
-void photoresCatch() {
-  
+//Fonction permettant de reccuperer la luminosite et d'adapter la temperature max et min
+void photoresCatch() {  
   photores = analogRead(A1); //Reccuperation du retour de la photoresistance
   //Si il y a beaucoup de luminosite (on estime qu'il fait jour)
   if(photores >= 930) {
@@ -135,6 +156,7 @@ void photoresCatch() {
   }
 }
 
+//Fonction permettant la reccuperation des donnes bluetooths
 void readSerialPort(){
  while (BTSerie.available()) {
    delay(10); 
@@ -143,7 +165,28 @@ void readSerialPort(){
      msg += c; //makes the string readString
    }
  }
- BTSerie.flush();
+ BTSerie.flush(); //Nettoyage du buffer
+}
+
+//Fonction permettant l'affichage sur l'ecran lcd
+void affichageLCD(int temp, bool chauffage) {
+   const char * degree = "\337C"; // °C
+  
+   lcd.begin(16,2); //Definit la taille de l'ecran
+   lcd.setCursor(0,0); //Place le curseur sur la premiere ligne
+   //Affichage de l'etat du chauffage
+   if(chauffage){
+       lcd.print("Chauffage allume");
+   } else {
+       lcd.print("Chauffage eteint");
+   }
+   //Affichage de la temperature
+   lcd.setCursor(0,10); //Place le curseur sur la seconde ligne
+   lcd.print("Temp : ");
+   lcd.print(temp); 
+   lcd.print(degree);
+
+   delay(2000); //Donne un delay de mise a jour
 }
 
 //------------------------------------------------------------
