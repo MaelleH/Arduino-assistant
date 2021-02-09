9f7ce71ff816095df7280cbf1cfcd48086c27dd4
MaelleH <h.maelle.98@gmail.com>
2021-02-09 09:25:43 +0100

    code soso

---
+++ main/code_lecture_son.ino.ino
@@ -0,0 +1,72 @@
+//Libraries 
+#include <SD.h>
+#include <TMRpcm.h>
+ 
+//Constants 
+#define SD_ChipSelectPin 11//4 
+const int speakerPin=9;
+char* mahalia="mahalia.wav";
+char* mario="mario.wav";
+
+
+//Variables
+unsigned long previousTime=0;
+unsigned long interval=1000;
+
+//Objects
+TMRpcm tmrpcm;
+
+/*******************************************************************
+ *                                MAIN
+*******************************************************************/
+void setup(){
+/* function setup */
+  Serial.begin(9600);
+  //Init sd shield
+  if (!SD.begin(SD_ChipSelectPin)) {Serial.println("SD fail"); return; }
+
+
+
+//Init speaker
+
+tmrpcm.speakerPin = speakerPin;
+
+tmrpcm.setVolume(3);
+
+}
+
+
+
+void loop(){
+
+/* function loop */
+
+if(millis()-previousTime>interval){
+
+activateOutput();
+
+previousTime=millis();
+
+}
+
+}
+
+/*******************************************************************
+
+ *                               FUNCTIONS
+
+ *******************************************************************/
+
+void activateOutput(){
+
+/* function activateOutput */
+
+Serial.println("Play sound");
+
+tmrpcm.play(file);
+//tone(9, 1000);
+while(tmrpcm.isPlaying()){}//wait until file is played
+
+//delay(1000);tmrpcm.stopPlayback(); // or wait 1sec and stop music
+
+}
